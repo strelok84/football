d08721784c1a8d79f219896de433a27e99558959
Andrey Bespalov <streloc84@gmail.com>
2021-02-07 14:53:29 +0400

    dateFrom

--- src/components/MathesOfTeam/Matches.js
+++ src/components/MathesOfTeam/Matches.js
@@ -1,17 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
 class Matches extends React.Component {
-  state = {
-    isLoading: true,
-    data: [],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      data: [],
+      dateFrom: "",
+      dateTo: "",
+    };
+    this.setDateFrom=this.setDateFrom.bind(this)
+    this.setDateTo=this.setDateTo.bind(this)
+  }
 
   async componentDidMount() {
-    const response = await fetch(
-      `https://api.football-data.org/v2/teams/${this.props.location.state.matchid}/matches?status=SCHEDULED
+    let response
+    if (this.state.dateFrom === "" && this.state.dateTo === "") {
+      response = await fetch(
+        `https://api.football-data.org/v2/teams/${this.props.location.state.matchid}/matches?dateTo/dateFrom=2021-01-01/2021-06-22
         `,
-      { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
-    );
+        { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
+      );
+    }
     const data = await response.json();
     console.log(data);
     this.setState({
@@ -20,13 +30,40 @@ class Matches extends React.Component {
     });
   }
 
+  setDateFrom(event) {
+    this.setState({ dateFrom: event.target.value })
+  }
+  setDateTo(event){
+    this.setState({ dateTo: event.target.value })
+  }
+
   render() {
     return (
+      <div>
+        <form onSubmit={this.dateFrom2}>
+          <input
+            type="date"
+            onChange={this.setDateFrom}
+            
+          />
+          <input type="submit" value="Найти" />
+        </form>
+        <form onSubmit={this.dateFrom2}>
+          <input
+            type="date"
+            onChange={this.setDateTo}
+            
+          />
+          <input type="submit" value="Найти" />
+        </form>
       <table className="table">
         <thead>
           <tr>
             <th>Матч</th>
-            <th>Дата матча по времени {Intl.DateTimeFormat().resolvedOptions().timeZone}</th>
+            <th>
+              Дата матча по времени{" "}
+              {Intl.DateTimeFormat().resolvedOptions().timeZone}
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -40,6 +77,7 @@ class Matches extends React.Component {
           ))}
         </tbody>
       </table>
+      </div>
     );
   }
 }
