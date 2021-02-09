38691a72eeb6de5313673f149fc42af72f6b870c
Andrey Bespalov <streloc84@gmail.com>
2021-02-09 18:21:39 +0400

    search for all

--- src/components/MatchesOfLigue/MatchesOfLigue.js
+++ src/components/MatchesOfLigue/MatchesOfLigue.js
@@ -1,16 +1,39 @@
 import React, { Component } from "react";
 
 class MatchesOfLigue extends React.Component {
-  state = {
-    isLoading: true,
-    data: [],
-  };
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      data: [],
+      dateFrom:"",
+      dateTo:""
+    }
+    this.setDateFrom = this.setDateFrom.bind(this)
+    this.setDateTo = this.setDateTo.bind(this)
+    this.searchDate = this.searchDate.bind(this)
+  }
   async componentDidMount() {
-    const response = await fetch(
-      `https://api.football-data.org/v2/competitions/${this.props.location.state.ligueId}/matches`,
-      { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
-    );
+    let response;
+    if(!this.state.dateFrom && !this.state.dateTo){
+      response = await fetch(
+        `https://api.football-data.org/v2/competitions/${this.props.location.state.ligueId}/matches?status=SCHEDULED`,
+        { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
+      );
+    }else if((!this.state.dateFrom && this.state.dateTo) || (this.state.dateFrom && !this.state.dateTo)){
+      alert("Нужно ввести обе даты")
+      response = await fetch(
+        `https://api.football-data.org/v2/competitions/${this.props.location.state.ligueId}/matches?status=SCHEDULED`,
+        { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
+      );
+    }else{
+      response = await fetch(
+        `https://api.football-data.org/v2/competitions/${this.props.location.state.ligueId}/matches?dateTo=${this.state.dateTo}&dateFrom=${this.state.dateFrom}`,
+        { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
+      );
+    }
+
+    
     const data = await response.json();
     console.log(data);
     this.setState({
@@ -19,29 +42,56 @@ class MatchesOfLigue extends React.Component {
     });
   }
 
+  setDateFrom(event) { 
+    this.setState({ dateFrom: event.target.value })
+  }
+  setDateTo(event) { 
+    this.setState({ dateTo: event.target.value })
+  }
+  searchDate(event) { 
+    event.preventDefault()
+    this.componentDidMount()
+  }
+
   render() {
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Команды</th>
-            <th>
-              Дата матча по времени{" "}
-              {Intl.DateTimeFormat().resolvedOptions().timeZone}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.data.map((item) => (
-            <tr key={item.id}>
-              <td>
-                {item.homeTeam.name} - {item.awayTeam.name}
-              </td>
-              <td>{new Date(item.utcDate).toLocaleString()}</td>
+      <div>
+        <form onSubmit={this.searchDate}>
+          <input
+            type="date"
+            onChange={this.setDateFrom}
+          />
+
+        </form>
+        <form onSubmit={this.searchDate}>
+          <input
+            type="date"
+            onChange={this.setDateTo}
+          />
+          <input type="submit" value="Найти" />
+        </form>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Команды</th>
+              <th>
+                Дата матча по времени{" "}
+                {Intl.DateTimeFormat().resolvedOptions().timeZone}
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.data.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  {item.homeTeam.name} - {item.awayTeam.name}
+                </td>
+                <td>{new Date(item.utcDate).toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
--- src/components/MathesOfTeam/Matches.js
+++ src/components/MathesOfTeam/Matches.js
@@ -9,74 +9,99 @@ class Matches extends React.Component {
       dateFrom: "",
       dateTo: "",
     };
-    this.setDateFrom=this.setDateFrom.bind(this)
-    this.setDateTo=this.setDateTo.bind(this)
+    this.setDateFrom = this.setDateFrom.bind(this)
+    this.setDateTo = this.setDateTo.bind(this)
+    this.searchDate = this.searchDate.bind(this)
   }
 
   async componentDidMount() {
-    let response
-    if (this.state.dateFrom === "" && this.state.dateTo === "") {
+    let response;
+
+    if (!this.state.dateFrom && !this.state.dateTo) {
       response = await fetch(
-        `https://api.football-data.org/v2/teams/${this.props.location.state.matchid}/matches?dateTo/dateFrom=2021-01-01/2021-06-22
-        `,
+        `https://api.football-data.org/v2/teams/${this.props.location.state.matchid}/matches?status=SCHEDULED`,
+        { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
+      );
+    } else if ((!this.state.dateFrom && this.state.dateTo) || (this.state.dateFrom && !this.state.dateTo)) {
+      alert("Нужно ввести обе даты")
+      response = await fetch(
+        `https://api.football-data.org/v2/teams/${this.props.location.state.matchid}/matches?status=SCHEDULED`,
+        { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
+      );
+    } else {
+      response = await fetch(
+        `https://api.football-data.org/v2/teams/${this.props.location.state.matchid}/matches?dateTo=${this.state.dateTo}&dateFrom=${this.state.dateFrom}`,
         { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
       );
     }
+
     const data = await response.json();
-    console.log(data);
-    this.setState({
-      isLoading: false,
-      data: data.matches,
-    });
+
+    if (response.ok) {
+        this.setState({
+        isLoading: false,
+        data: data.matches,
+      })
+    } else {
+      this.setState({
+        isLoading: false,
+        data: [{ homeTeam: { name: "Нет данных в этом диапазоне" }, awayTeam: { name: "доступны данные только за последние 750 дней" }, id: "Нет данных", utcDate: Date.now() }],
+      })
+    }
   }
 
   setDateFrom(event) {
     this.setState({ dateFrom: event.target.value })
   }
-  setDateTo(event){
+  setDateTo(event) {
     this.setState({ dateTo: event.target.value })
+  }
+
+  searchDate(event) {
+    event.preventDefault()
+    this.componentDidMount()
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.dateFrom2}>
+        <form onSubmit={this.searchDate}>
           <input
             type="date"
             onChange={this.setDateFrom}
-            
+
           />
-          <input type="submit" value="Найти" />
+
         </form>
-        <form onSubmit={this.dateFrom2}>
+        <form onSubmit={this.searchDate}>
           <input
             type="date"
             onChange={this.setDateTo}
-            
+
           />
           <input type="submit" value="Найти" />
         </form>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Матч</th>
-            <th>
-              Дата матча по времени{" "}
-              {Intl.DateTimeFormat().resolvedOptions().timeZone}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.data.map((item) => (
-            <tr key={item.id}>
-              <td>
-                {item.homeTeam.name} - {item.awayTeam.name}
-              </td>
-              <td>{new Date(item.utcDate).toLocaleString()}</td>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Матч</th>
+              <th>
+                Дата матча по времени{" "}
+                {Intl.DateTimeFormat().resolvedOptions().timeZone}
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.data.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  {item.homeTeam.name} - {item.awayTeam.name}
+                </td>
+                <td>{new Date(item.utcDate).toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
