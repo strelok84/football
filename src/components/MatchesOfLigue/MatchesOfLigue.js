68fd1a6a4596e0efa752d42cd9558e74b04d4da9
Andrey Bespalov <streloc84@gmail.com>
2021-03-27 14:32:46 +0400

    del_constructor

--- src/components/MatchesOfLigue/MatchesOfLigue.js
+++ src/components/MatchesOfLigue/MatchesOfLigue.js
@@ -1,38 +1,30 @@
-import React from "react"
-import Loader from "../Loader/Loader"
-import { withRouter } from "react-router-dom"
+import React from "react";
+import Loader from "../Loader/Loader";
+import { withRouter } from "react-router-dom";
 
-// Календарь лиги - список матчей лиги. По умолчанию показаны будущие матчи. 
+// Календарь лиги - список матчей лиги. По умолчанию показаны будущие матчи.
 //Есть поиск матчей за конкретный период. Введенные данные сохраняются в sessionStorage, при обновлении страницы соответственно результаты поиска не теряются
 
-
 class MatchesOfLigue extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isLoading: true,
-      data: [],
-      dateFrom: "",
-      dateTo: ""
-    }
-    this.setDateFrom = this.setDateFrom.bind(this)
-    this.setDateTo = this.setDateTo.bind(this)
-    this.searchDate = this.searchDate.bind(this)
-  }
-
+  state = {
+    isLoading: true,
+    data: [],
+    dateFrom: "",
+    dateTo: "",
+  };
   async componentDidMount() {
-
-    let id = localStorage.getItem("ligueId") || this.props.location.state.ligueId
-    let dateTo = sessionStorage.getItem("dateTo") || this.state.dateTo
-    let dateFrom = sessionStorage.getItem("dateFrom") || this.state.dateFrom
-    let response
+    let id =
+      localStorage.getItem("ligueId") || this.props.location.state.ligueId;
+    let dateTo = sessionStorage.getItem("dateTo") || this.state.dateTo;
+    let dateFrom = sessionStorage.getItem("dateFrom") || this.state.dateFrom;
+    let response;
     if (!dateFrom && !dateTo) {
       response = await fetch(
         `https://api.football-data.org/v2/competitions/${id}/matches?status=SCHEDULED`,
         { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
       );
     } else if ((!dateFrom && dateTo) || (dateFrom && !dateTo)) {
-      alert("Нужно ввести обе даты")
+      alert("Нужно ввести обе даты");
       response = await fetch(
         `https://api.football-data.org/v2/competitions/${id}/matches?status=SCHEDULED`,
         { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
@@ -41,10 +33,13 @@ class MatchesOfLigue extends React.Component {
       response = await fetch(
         `https://api.football-data.org/v2/competitions/${id}/matches?dateTo=${dateTo}&dateFrom=${dateFrom}`,
         { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
-      )
-      window.history.pushState(null, null, "matchesOfLigue?" + dateFrom + "--" + dateTo)
+      );
+      window.history.pushState(
+        null,
+        null,
+        "matchesOfLigue?" + dateFrom + "--" + dateTo
+      );
     }
-
 
     const data = await response.json();
 
@@ -54,37 +49,38 @@ class MatchesOfLigue extends React.Component {
     });
   }
 
-  componentWillUnmount() {
-    sessionStorage.clear()
-  }
+  componentWillUnmount = () => {
+    sessionStorage.clear();
+  };
 
-  setDateFrom(event) {
-    this.setState({ dateFrom: event.target.value })
-    sessionStorage.setItem("dateFrom", event.target.value)
-  }
-  setDateTo(event) {
-    this.setState({ dateTo: event.target.value })
-    sessionStorage.setItem("dateTo", event.target.value)
-  }
-  searchDate(event) {
-    event.preventDefault()
-    this.componentDidMount()
-  }
+  setDateFrom = (event) => {
+    this.setState({ dateFrom: event.target.value });
+    sessionStorage.setItem("dateFrom", event.target.value);
+  };
+  setDateTo = (event) => {
+    this.setState({ dateTo: event.target.value });
+    sessionStorage.setItem("dateTo", event.target.value);
+  };
+  searchDate = (event) => {
+    event.preventDefault();
+    this.componentDidMount();
+  };
 
   render() {
-
     return (
-      <div className="container" >
+      <div className="container">
         <div className="mt-2 mb-2">
-          <a className="mr-3" href="https://strelok84.github.io/football/">Главная страница</a>
-          <span>{'\u00B7'}</span>
+          <a className="mr-3" href="https://strelok84.github.io/football/">
+            Главная страница
+          </a>
+          <span>{"\u00B7"}</span>
           <span className="ml-3">Календарь лиги</span>
         </div>
 
         <form className="form-inline mt-3" onSubmit={this.searchDate}>
           <label>
             От:
-           <input
+            <input
               className="form-control ml-1 mr-3"
               type="date"
               onChange={this.setDateFrom}
@@ -94,16 +90,22 @@ class MatchesOfLigue extends React.Component {
         <form className="form-inline mt-2 mb-3" onSubmit={this.searchDate}>
           <label>
             До:
-          <input
+            <input
               className="form-control ml-1 mr-3 "
               type="date"
               onChange={this.setDateTo}
             />
           </label>
-          <input className="form-control btn btn-primary" type="submit" value="Найти" />
+          <input
+            className="form-control btn btn-primary"
+            type="submit"
+            value="Найти"
+          />
         </form>
 
-        {this.state.isLoading ? <Loader /> :
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
           <table className="table">
             <thead>
               <tr>
@@ -125,11 +127,10 @@ class MatchesOfLigue extends React.Component {
               ))}
             </tbody>
           </table>
-        }
-
+        )}
       </div>
     );
   }
 }
 
-export default withRouter(MatchesOfLigue)
+export default withRouter(MatchesOfLigue);
--- src/components/MathesOfTeam/Matches.js
+++ src/components/MathesOfTeam/Matches.js
@@ -1,5 +1,5 @@
-import React from "react"
-import Loader from "../Loader/Loader"
+import React from "react";
+import Loader from "../Loader/Loader";
 /*
 Календарь конкретной команды. По умолчанию выводятся будущие матчи. 
 Можно вывести матчи за конкретный период (если период недоступен - выводит соответствующую надпись).
@@ -9,31 +9,26 @@ import Loader from "../Loader/Loader"
 */
 
 class Matches extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      data: [],
-      dateFrom: "",
-      dateTo: "",
-    };
-    this.setDateFrom = this.setDateFrom.bind(this)
-    this.setDateTo = this.setDateTo.bind(this)
-    this.searchDate = this.searchDate.bind(this)
-  }
-
+  state = {
+    isLoading: true,
+    data: [],
+    dateFrom: "",
+    dateTo: "",
+  };
   async componentDidMount() {
-    let response
-    let matchid = localStorage.getItem("matchId") || this.props.location.state.matchId
-    let dateTo = sessionStorage.getItem("teamDateTo") || this.state.dateTo
-    let dateFrom = sessionStorage.getItem("teamDateFrom") || this.state.dateFrom
+    let response;
+    let matchid =
+      localStorage.getItem("matchId") || this.props.location.state.matchId;
+    let dateTo = sessionStorage.getItem("teamDateTo") || this.state.dateTo;
+    let dateFrom =
+      sessionStorage.getItem("teamDateFrom") || this.state.dateFrom;
     if (!dateFrom && !dateTo) {
       response = await fetch(
         `https://api.football-data.org/v2/teams/${matchid}/matches?status=SCHEDULED`,
         { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
       );
     } else if ((!dateFrom && dateTo) || (dateFrom && !dateTo)) {
-      alert("Нужно ввести обе даты")
+      alert("Нужно ввести обе даты");
       response = await fetch(
         `https://api.football-data.org/v2/teams/${matchid}/matches?status=SCHEDULED`,
         { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
@@ -42,59 +37,76 @@ class Matches extends React.Component {
       response = await fetch(
         `https://api.football-data.org/v2/teams/${matchid}/matches?dateTo=${dateTo}&dateFrom=${dateFrom}`,
         { headers: { "X-Auth-Token": "a3b3685ba5fd4c8685be0540c85652f2" } }
-      )
-      window.history.pushState(null, null, "matches?" + dateFrom + "--" + dateTo)
+      );
+      window.history.pushState(
+        null,
+        null,
+        "matches?" + dateFrom + "--" + dateTo
+      );
     }
 
-
-    const data = await response.json()
+    const data = await response.json();
 
     if (response.ok) {
       this.setState({
         isLoading: false,
         data: data.matches,
-      })
+      });
     } else {
       this.setState({
         isLoading: false,
-        data: [{ homeTeam: { name: "Нет данных в этом диапазоне" }, awayTeam: { name: "доступны данные только за последние 750 дней" }, id: "Нет данных", utcDate: Date.now() }],
-      })
+        data: [
+          {
+            homeTeam: { name: "Нет данных в этом диапазоне" },
+            awayTeam: { name: "доступны данные только за последние 750 дней" },
+            id: "Нет данных",
+            utcDate: Date.now(),
+          },
+        ],
+      });
     }
   }
 
   componentWillUnmount() {
-    sessionStorage.clear()
+    sessionStorage.clear();
   }
 
-  setDateFrom(event) {
-    this.setState({ dateFrom: event.target.value })
-    sessionStorage.setItem("teamDateFrom", event.target.value)
-  }
-  setDateTo(event) {
-    this.setState({ dateTo: event.target.value })
-    sessionStorage.setItem("teamDateTo", event.target.value)
-  }
+  setDateFrom = (event) => {
+    this.setState({ dateFrom: event.target.value });
+    sessionStorage.setItem("teamDateFrom", event.target.value);
+  };
+  setDateTo = (event) => {
+    this.setState({ dateTo: event.target.value });
+    sessionStorage.setItem("teamDateTo", event.target.value);
+  };
 
-  searchDate(event) {
-    event.preventDefault()
-    this.componentDidMount()
-  }
+  searchDate = (event) => {
+    event.preventDefault();
+    this.componentDidMount();
+  };
 
   render() {
     return (
       <div className="container">
-        <div className="mt-2 mb-2" >
-          <a className="mr-3" href="https://strelok84.github.io/football/">Главная страница</a>
-          <span>{'\u00B7'}</span>
-          <a href="https://strelok84.github.io/football/teams" className="ml-3 mr-3">Команды лиги</a>
-          <span>{'\u00B7'}</span>
+        <div className="mt-2 mb-2">
+          <a className="mr-3" href="https://strelok84.github.io/football/">
+            Главная страница
+          </a>
+          <span>{"\u00B7"}</span>
+          <a
+            href="https://strelok84.github.io/football/teams"
+            className="ml-3 mr-3"
+          >
+            Команды лиги
+          </a>
+          <span>{"\u00B7"}</span>
           <span className="ml-3">Календарь команды</span>
         </div>
 
         <form className="form-inline mt-3" onSubmit={this.searchDate}>
           <label>
             От:
-           <input
+            <input
               className="form-control ml-1 mr-3"
               type="date"
               onChange={this.setDateFrom}
@@ -104,16 +116,22 @@ class Matches extends React.Component {
         <form className="form-inline mt-2 mb-3" onSubmit={this.searchDate}>
           <label>
             До:
-          <input
+            <input
               className="form-control ml-1 mr-3 "
               type="date"
               onChange={this.setDateTo}
             />
           </label>
-          <input className="form-control btn btn-primary" type="submit" value="Найти" />
+          <input
+            className="form-control btn btn-primary"
+            type="submit"
+            value="Найти"
+          />
         </form>
 
-        {this.state.isLoading ? <Loader /> :
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
           <table className="table">
             <thead>
               <tr>
@@ -135,8 +153,7 @@ class Matches extends React.Component {
               ))}
             </tbody>
           </table>
-        }
-
+        )}
       </div>
     );
   }
